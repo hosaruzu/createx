1d2006cfb9ad0d037b5b6d2a5af08469f0a963e5
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-06-14 19:41:20 +0400

    feat: add service page blocks: offer, how-we-do

---
+++ src/js/components/accordions.js
@@ -0,0 +1,9 @@
+if (document.querySelector(".offer")) {
+  const accordion1 = new Accordion(".accordion-1", {
+    speed: 300,
+  });
+
+  const accordion2 = new Accordion(".accordion-2", {
+    speed: 300,
+  });
+}
